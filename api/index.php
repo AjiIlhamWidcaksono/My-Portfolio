2c56507a91ea896abd4854e67f5d0b2f264564c5
aji <auliabunganastiti@gmail.com>
2026-07-05 16:24:04 +0700

    Fix: Create temporary /tmp folders dynamically on runtime in api/index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,17 @@
 <?php
 
+// Membuat folder penampung temporary di /tmp secara dinamis jika belum ada
+$required_directories = [
+    '/tmp/views',
+    '/tmp/cache',
+    '/tmp/sessions'
+];
+
+foreach ($required_directories as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
 // Teruskan permintaan dari Vercel ke file index.php Laravel di folder public
 require __DIR__ . '/../public/index.php';
